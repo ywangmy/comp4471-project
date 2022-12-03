eb355c63909942f983838c71d834349da711e159
zory233 <yzhanglo@connect.ust.hk>
2022-12-04 01:31:59 +0800

    change into yaml era

--- comp4471/util.py
+++ comp4471/util.py
@@ -1,4 +1,17 @@
 from typing import List, Tuple
+import random
+import numpy as np
+
+import torch
+def setup_seed(seed = 4471):
+        np.random.seed(seed)
+        random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        # https://zhuanlan.zhihu.com/p/73711222
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
 
--- dataloader/loader.py
+++ dataloader/loader.py
@@ -4,45 +4,46 @@ from .dataset import DfdcDataset
 from .augment import create_transforms_train, create_transforms_val
 
 # https://pytorch.org/docs/stable/data.html
-def configure_data(args, config):
+def configure_data(cfg):
     # Transforms (augmentation, converting img to tensor, etc.)
-    trans_train = create_transforms_train(config['size'])
-    trans_val = create_transforms_val(config['size'])
+    trans_train = create_transforms_train(cfg['dataset']['size'])
+    trans_val = create_transforms_val(cfg['dataset']['size'])
 
     # Map-style dataset
     data_train = DfdcDataset(mode='train',
-                             root_dir=args.root_dir,
-                             crops_dir=args.crops_dir,
-                             fold=args.fold,
-                             folds_csv_path=args.folds_csv_path,
-                             folds_json_path=args.folds_json_path,
-                             trans=trans_train)
+                            root_dir=cfg['dataset']['root_dir'],
+                            crops_dir=cfg['dataset']['crops_dir'],
+                            fold=cfg['dataset']['fold'],
+                            folds_csv_path=cfg['dataset']['folds_csv_path'],
+                            folds_json_path=cfg['dataset']['folds_json_path'],
+                            trans=trans_train)
     data_val = DfdcDataset(mode='val',
-                         root_dir=args.root_dir,
-                         crops_dir=args.crops_dir,
-                         fold=args.fold,
-                         folds_csv_path=args.folds_csv_path,
-                         folds_json_path=args.folds_json_path,
+                         root_dir=cfg['dataset']['root_dir'],
+                         crops_dir=cfg['dataset']['crops_dir'],
+                         fold=cfg['dataset']['fold'],
+                         folds_csv_path=cfg['dataset']['folds_csv_path'],
+                         folds_json_path=cfg['dataset']['folds_json_path'],
                          trans=trans_val)
 
     # Sampler
-    if args.is_distributed:
-        sampler_train = torch.utils.data.distributed.DistributedSampler(dataset=data_train,shuffle=True)
-    else: sampler_train = None
+    if cfg['distributed']['toggle']:
+        sampler_train = torch.utils.data.distributed.DistributedSampler(dataset=data_train, shuffle=True)
+    else:
+        sampler_train = None
 
     # Loader
     loader_train = DataLoader(data_train,
-                              batch_size=config['optimizer']['batch_size'],
+                              batch_size=cfg['optimizer']['batch_size'],
                               shuffle=sampler_train is None,
                               sampler=sampler_train,
                               pin_memory=False,
-                              num_workers=args.workers,
+                              num_workers=cfg['dataset']['load_workers'],
                               )
 
     loader_val = DataLoader(data_val,
-                            batch_size=config['optimizer']['batch_size'] * 2, # less memory consumption
+                            batch_size=cfg['optimizer']['batch_size'] * 2, # less memory consumption
                             shuffle=False,
                             pin_memory=True,
-                            num_workers=args.workers,
+                            num_workers=cfg['dataset']['load_workers'],
                             )
-    return sampler_train, loader_train, loader_val
+    return loader_train, loader_val
