7057b320eaf2d30ae5d5d2bd37606ff3aaf3c0ce
yc wang <ywangmy@connect.ust.hk>
2022-12-05 09:44:34 +0800

    pre

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,15 +1,15 @@
-DATA_ROOT=$1
+DATA_ROOT=$1 CROPS_ROOT=$2 OUT=$3
 echo "Extracting bounding boxes from original videos"
 python preprocessing/finding_face_region_using_real.py --video-dir $DATA_ROOT
 
 echo "Extracting crops as pngs"
-python preprocessing/extract_crops.py --video-dir $DATA_ROOT --crops-dir crops
+python preprocessing/extract_crops.py --video-dir $DATA_ROOT --crops-dir $CROPS_ROOT
 
 echo "Extracting landmarks"
-python preprocessing/generate_landmarks.py --video-dir $DATA_ROOT
+python preprocessing/generate_landmarks.py --video-dir $DATA_ROOT --crops-dir $CROPS_ROOT
 
 echo "Extracting SSIM masks"
-python preprocessing/generate_diffs.py --video-dir $DATA_ROOT
+python preprocessing/generate_diffs.py --video-dir $DATA_ROOT --crops-dir $CROPS_ROOT
 
 echo "Generate folds"
-python preprocessing/generate_folds.py --root-dir $DATA_ROOT
+python preprocessing/generate_folds.py --root-dir $DATA_ROOT --crops-dir $CROPS_ROOT --out $OUT
